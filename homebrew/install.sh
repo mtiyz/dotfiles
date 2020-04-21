ba2e6098f8bdb84e18a65d857caecece706077a9
motiya <54768914+mtiyz@users.noreply.github.com>
2020-04-21 15:55:50 +0900

    :recycle: change the way Paths are set up

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,7 +1,12 @@
 git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
 mkdir ~/.linuxbrew/bin
 ln -sfv ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin
-export PATH="$HOME/.linuxbrew/bin:$PATH"
+
+if [ -f $HOME/.profile ]; then
+  sed -e '$a export PATH="$HOME/.linuxbrew/bin:$PATH"' $HOME/.profile -i
+else
+  echo export PATH="$HOME/.linuxbrew/bin:$PATH" > $HOME/.profile
+fi
 
 brew install gcc
 brew install sudo
