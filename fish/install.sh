f0970174acb2e3d6e742c64e25c99c392fc950ad
motiya <54768914+mtiyz@users.noreply.github.com>
2020-04-21 17:01:38 +0900

    :bug: fix bugs

--- fish/install.sh
+++ fish/install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+DOTPATH=~/dotfiles/
+
 export PATH="$HOME/.linuxbrew/bin:$PATH"
 
 brew install fish
 
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
-/usr/local/bin/fish fisher add rafaelrinaldi/pure
+fish fisher add rafaelrinaldi/pure
 
 rm ~/.config/fish/config.fish
-mv config.fish ~/.config/fish/config.fish
+mv $DOTPATH/fish/config.fish ~/.config/fish/config.fish
 
-sudo chsh -s /usr/local/bin/fish
+sudo chsh -s `which fish`
