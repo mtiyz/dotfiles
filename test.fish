14b0b25aff5622b556aa8cf97e5919a5e2aa4beb
motiya <54768914+mtiyz@users.noreply.github.com>
2020-05-11 15:00:44 +0900

    :white_check_mark: fix test.sh

--- test.fish
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env fish
-
-dot update
-dot install git-credential-helper
-dot install node
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+fish -c "dot update"
+fish -c "dot install git-credential-helper"
+fish -c "dot install node"
