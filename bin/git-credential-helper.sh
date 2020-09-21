db615e1711820596c07caae4788a0459b637548e
noroz <54768914+norozic@users.noreply.github.com>
2020-09-21 12:33:20 +0900

    🐛 fix shell script

--- bin/git-credential-helper.sh
+++ bin/git-credential-helper.sh
@@ -6,18 +6,18 @@ if uname -a | grep "microsoft"; then
   ## WSL
   git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-wincred.exe"
   exit 0
-else if type "apt" > /dev/null 2>&1; then
+elif type "apt" > /dev/null 2>&1; then
   ## Ubuntu
   sudo apt install libsecret-1-0 libsecret-1-dev
   cd /usr/share/doc/git/contrib/credential/libsecret
   sudo make
   git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
   exit 0
-else if type "yum" > /dev/null 2>&1; then
+elif type "yum" > /dev/null 2>&1; then
   ## RHEL
   git config --global credential.helper /usr/libexec/git-core/git-credential-libsecret
   exit 0
-else if type "pacman" > /dev/null 2>&1; then
+elif type "pacman" > /dev/null 2>&1; then
   ## ArchLinux
   git config --global credential.helper /usr/lib/git-core/git-credential-libsecret
   exit 0
